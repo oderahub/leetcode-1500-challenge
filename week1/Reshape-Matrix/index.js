6e9c522819cbe8eee9249209ce2f079dd85e6909
oderahub <okere_chidera@yahoo.com>
2025-03-26 13:30:57 +0100

    chore: added transpose matrix

--- week1/Reshape-Matrix/index.js
+++ week1/Reshape-Matrix/index.js
@@ -9,13 +9,13 @@
 // Example 1:
 
 
-Input: matrix = [[1, 2], [3, 4]], r = 1, c = 4
-Output: [[1, 2, 3, 4]]
-// Example 2:
+// Input: matrix = [[1, 2], [3, 4]], r = 1, c = 4
+// Output: [[1, 2, 3, 4]]
+// // Example 2:
 
 
-Input: matrix = [[1, 2], [3, 4]], r = 2, c = 4
-Output: [[1, 2], [3, 4]]
+// Input: matrix = [[1, 2], [3, 4]], r = 2, c = 4
+// Output: [[1, 2], [3, 4]]
 
 
 Constraints:
@@ -47,5 +47,46 @@ var matrixReshape = (matrix, r, c) => {
     return reshaped
 }
 
-console.log(matrixReshape([[1, 2], [3, 4]], 1, 4));  // Output: [[1, 2, 3, 4]]
-console.log(matrixReshape([[1, 2], [3, 4]], 2, 4));
+//console.log(matrixReshape([[1, 2], [3, 4]], 1, 4));  // Output: [[1, 2, 3, 4]]
+//console.log(matrixReshape([[1, 2], [3, 4]], 2, 4));
+
+
+// Given a 2D integer array matrix, return the transpose of matrix.
+
+// The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+// Example 1:
+
+// Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+// Example 2:
+
+// Input: matrix = [[1, 2, 3], [4, 5, 6]]
+// Output: [[1, 4], [2, 5], [3, 6]]
+
+// solution
+
+var transpose = function (matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let newMatrix = []
+
+    // create an appropirate matrix size
+
+    for (let i = 0; i < n; i++) {
+        newMatrix[i] = []
+    }
+
+    // transposing the values 
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            newMatrix[j][i] = matrix[i][j]
+        }
+    }
+    return newMatrix
+}
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+console.log(transpose(matrix));
